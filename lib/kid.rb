053619194e1bfb5f547846e587406826636dcc21
Victor Plushko <vicplushko33@gmail.com>
2020-04-22 16:47:52 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -5,7 +5,7 @@ require_relative './fancy_dance.rb'
 
 class Kid
   include FancyDance::InstanceMethods
-  extend 
+  extend FancyDance::ClassMethods
   attr_accessor :name
   
   def initialize(name)
